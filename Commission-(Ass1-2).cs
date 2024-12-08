d348f576d48b8e406964b1e6a39687fc3f398beb
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2024-12-08 19:07:33 +0530

    Add files via upload

---
+++ Commission-(Ass1-2).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_programs
+{
+    class Commission
+    {
+        static void Main()
+        {
+            // Accept amount from the user
+            Console.Write("Enter the amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            double commission = 0;
+
+            // Calculate commission if amount is greater than 20000
+            if (amount > 20000)
+            {
+                commission = amount * 0.03;
+            }
+
+            // Display amount and commission
+            Console.WriteLine("Amount: " + amount);
+            Console.WriteLine("Commission: " + commission);
+        }
+    }
+
+}
