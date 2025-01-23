e0ac4c2c9eef5fae59b4cf7e73a569f5acf19f3e
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2025-01-23 22:19:27 +0530

    Add files via upload

---
+++ Employee-(Ass4-9).cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_programs
+{
+    // Base class
+    class Employee
+    {
+        public string name;
+        public int id;
+
+        public Employee(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine("Name: "+name+", ID: "+id);
+        }
+    }
+
+    // Derived class: Programmer
+    class Programmer : Employee
+    {
+        public string programmingLanguage;
+
+        public Programmer(string name, int id, string programmingLanguage) : base(name, id) 
+        {
+            this.programmingLanguage = programmingLanguage;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("Programmer Information:");
+            Console.WriteLine("Name: " + name + ", ID: " + id+", Programming Language: "+programmingLanguage);
+        }
+    }
+
+    // Derived class: Manager
+    class Manager : Employee
+    {
+        public int teamSize;
+
+        public Manager(string name, int id, int teamSize) : base(name, id)
+        {
+            this.teamSize = teamSize;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Manager Information:");
+            Console.WriteLine("Name: " + name + ", ID: " + id+", Team Size: "+teamSize);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Employee Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Employee ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Employee employee = new Employee(name, id);
+
+            Console.Write("Enter Programming Language: ");
+            string programmingLanguage = Console.ReadLine();
+
+            Programmer programmer = new Programmer(name, id,programmingLanguage);
+
+            Console.Write("Team Size: ");
+            int teamSize = int.Parse(Console.ReadLine());
+
+            Manager manager = new Manager(name, id, teamSize);
+            
+            programmer.Display();
+            Console.WriteLine();
+            manager.Display();
+        }
+    }
+}
